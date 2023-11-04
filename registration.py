ddfa739e33301431cd181224555f6c733b53ab40
jennmcniff <jennifermcniff9@gmail.com>
2023-11-04 14:36:24 -0400

    moving to selecting courses

--- registration.py
+++ registration.py
@@ -2,13 +2,16 @@ import streamlit as st
 import pandas as pd
 import csv
 
+#implementing w/o streamlit, will encorporate streamlit later
+
 #performs keyword search on dataset
 def keyWordSearch():
+    #STREAMLIT: textbox
     keyWords = str(input("Enter three keywords: "))
+    #
     keyList = list(keyWords.split(" "))
     #vv csv data source file vv
-
-    courseOfferings = list() #= pd.read_csv("csdata.csv") #= list()
+    courseOfferings = list()
     with open("csdata.csv", "r") as f:
         reading = csv.reader(f,delimiter=",")
         for row in reading:
@@ -18,23 +21,26 @@ def keyWordSearch():
         print(each)
         for course in courseOfferings:
             print(course[2])
-            #course[2] is course title
             #if current keyword is within course title and course isn't already in courseList
-            #print(course[2].lower().find(each) != -1)
-            #print(course not in courseList)
             if (each in course[2].lower()) and (course not in courseList):
-                print("rel")
                 #relevantData = [title, crn, course #, days, timeslot, prof]
                 relevantData = list([course[2], course[0], course[1], course[5], course[6], course[8]])
                 courseList.append(relevantData)
-    print(courseList)
+    #print(courseList)
     return courseList
 
+def parseResults(potentialCourses):
+    #STREAMLIT
+    print("select a potential course")
+    for each in potentialCourses:
+        print(each)
+    #
+    #user select course
 
 
 def main():
-    print("starting out")
     potentialCourses = keyWordSearch()
+    parseResults(potentialCourses)
 
 
 main()

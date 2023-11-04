1c38d713918b0580c2556a05536bcbbaa20e4b21
jennmcniff <jennifermcniff9@gmail.com>
2023-11-04 14:26:57 -0400

    keyword search done

--- registration.py
+++ registration.py
@@ -1,22 +1,33 @@
 import streamlit as st
 import pandas as pd
+import csv
 
 #performs keyword search on dataset
-#adds 
 def keyWordSearch():
     keyWords = str(input("Enter three keywords: "))
     keyList = list(keyWords.split(" "))
     #vv csv data source file vv
-    courseOfferings = list()
+
+    courseOfferings = list() #= pd.read_csv("csdata.csv") #= list()
+    with open("csdata.csv", "r") as f:
+        reading = csv.reader(f,delimiter=",")
+        for row in reading:
+            courseOfferings.append(row)
     courseList = list()
     for each in keyList:
+        print(each)
         for course in courseOfferings:
-            #course[3] should be course title
+            print(course[2])
+            #course[2] is course title
             #if current keyword is within course title and course isn't already in courseList
-            if (each in course[3]) and (course not in courseList):
+            #print(course[2].lower().find(each) != -1)
+            #print(course not in courseList)
+            if (each in course[2].lower()) and (course not in courseList):
+                print("rel")
                 #relevantData = [title, crn, course #, days, timeslot, prof]
-                relevantData = list(course[3], course[0], course[1], course[5], course[6], course[8])
+                relevantData = list([course[2], course[0], course[1], course[5], course[6], course[8]])
                 courseList.append(relevantData)
+    print(courseList)
     return courseList
 
 
